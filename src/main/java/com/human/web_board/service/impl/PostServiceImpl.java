ce3bfc39bec3f1e78fa5d111cd6832c07fbd6197
jks2024 <jks2024@gmail.com>
2025-10-30 16:33:09 +0900

    10월30일 목 16:33

--- src/main/java/com/human/web_board/service/impl/PostServiceImpl.java
+++ src/main/java/com/human/web_board/service/impl/PostServiceImpl.java
@@ -6,6 +6,8 @@ import com.human.web_board.dto.PostCreateReq;
 import com.human.web_board.dto.PostRes;
 import com.human.web_board.service.PostService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +15,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PostServiceImpl implements PostService {
     private final PostDao postDao;
     private final MemberDao memberDao;
@@ -42,6 +45,11 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public boolean delete(Long id) {
-        return postDao.delete(id);
+        try {
+            return postDao.delete(id);
+        } catch (DataAccessException e) {
+            log.error("댓글 삭제 예외 발생: {}", e.getCause());
+            throw new IllegalArgumentException("댓글을 삭제 할 수 없습니다.");
+        }
     }
 }
