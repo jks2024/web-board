ebeeb4281b6a4f0a6e195c984bf9ad4cb31da43b
jks2024 <jks2024@gmail.com>
2025-10-31 15:29:52 +0900

    10월31일 오후 3:29

---
+++ src/main/java/com/human/web_board/controller/MemberController.java
@@ -0,0 +1,16 @@
+package com.human.web_board.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class MemberController {
+    // 회원 가입 폼 페이지
+
+    // 회원 가입 처리
+
+    // 회원 조회
+}
