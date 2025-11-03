ec945aa0089502e617178af141dccfb0b840c00e
jks2024 <jks2024@gmail.com>
2025-11-03 10:57:59 +0900

    11월3일 월요일

--- src/main/java/com/human/web_board/controller/MemberController.java
+++ src/main/java/com/human/web_board/controller/MemberController.java
@@ -1,16 +1,42 @@
 package com.human.web_board.controller;
 
+import com.human.web_board.dto.MemberSignupReq;
+import com.human.web_board.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/members")
 public class MemberController {
+    private final MemberService memberService;  // 의존성 주입
     // 회원 가입 폼 페이지
+    @GetMapping("/new")
+    public String signupForm(Model model) {
+        // model은 화면 정보 공유를 위해서 사용, 회원 정보를 담을 빈 객체를 생성 후 전달
+        model.addAttribute("memberForm", new MemberSignupReq());
+        return "members/new";
+    }
 
     // 회원 가입 처리
+    @PostMapping("/new")
+    public String signup(MemberSignupReq req, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) return "members/new";
+        try {
+            memberService.signup(req);
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("error", e.getStackTrace());
+            return "members/new";
+        }
+        return "redirect:/";  // 가입이 성공하면 로그인 페이지로 이동
+    }
 
     // 회원 조회
 }
--- src/main/java/com/human/web_board/controller/PostController.java
+++ src/main/java/com/human/web_board/controller/PostController.java
@@ -9,6 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -19,7 +21,7 @@ import java.util.List;
 @Slf4j
 public class PostController {
     private final PostService postService;
-    @GetMapping
+    @GetMapping  // 게시글 목록 가져 오기
     public String list(HttpSession session, Model model) {
         // 로그인 여부 확인 var은 타입 추론을 해서 자동으로 형을 찾아 줌
         var loginMember = session.getAttribute("loginMember");
@@ -28,4 +30,21 @@ public class PostController {
         model.addAttribute("posts", list);
         return "post/list";
     }
+
+    // 게시글 쓰기 폼
+    @GetMapping("/new")
+    public String postWriteForm(HttpSession session, Model model) {
+        return "posts/new";
+    }
+
+
+    // 게시글 쓰기 DB 처리
+
+
+    // 게시글 상세 조회
+    @GetMapping("/{postId}")
+    public String detail(@PathVariable Long postId, Model model, HttpSession session) {
+        return "posts/detail";
+    }
+
 }
